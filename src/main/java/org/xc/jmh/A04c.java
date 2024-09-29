b8a40f5206c4313b84ccf12d713df782fd7a97ad
Rene Schwietzke <r.schwietzke@xceptance.com>
2024-09-29 17:45:25 +0200

    misc

---
+++ src/main/java/org/xc/jmh/A04c.java
@@ -0,0 +1,52 @@
+package org.xc.jmh;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openjdk.jmh.annotations.Benchmark;
+import org.openjdk.jmh.annotations.BenchmarkMode;
+import org.openjdk.jmh.annotations.Fork;
+import org.openjdk.jmh.annotations.Measurement;
+import org.openjdk.jmh.annotations.Mode;
+import org.openjdk.jmh.annotations.OutputTimeUnit;
+import org.openjdk.jmh.annotations.Scope;
+import org.openjdk.jmh.annotations.Setup;
+import org.openjdk.jmh.annotations.State;
+import org.openjdk.jmh.annotations.Warmup;
+
+/**
+ * The simplest JMH benchmark
+ * Produce a result!
+ */
+@Fork(1)
+@BenchmarkMode(Mode.AverageTime)
+@OutputTimeUnit(TimeUnit.NANOSECONDS)
+@State(Scope.Thread)
+public class A04c
+{
+	long time;
+	Long lTime;
+
+	@Setup
+	public void setup()
+	{
+		time = System.currentTimeMillis();
+		lTime = Long.valueOf(time);
+	}
+
+	@Warmup(iterations = 3, time = 1, timeUnit = TimeUnit.SECONDS)
+	@Measurement(iterations = 3, time = 1, timeUnit = TimeUnit.SECONDS)
+	@Benchmark
+	public long addShort()
+	{
+		return 1 + time;
+	}
+
+	@Warmup(iterations = 5, time = 2, timeUnit = TimeUnit.SECONDS)
+	@Measurement(iterations = 10, time = 1, timeUnit = TimeUnit.SECONDS)
+	@Benchmark
+	public long addLong()
+	{
+		return 1 + time;
+	}
+
+}
