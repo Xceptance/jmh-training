1f6384e51b551b690692fd285424010e28b711c9
Rene Schwietzke <r.schwietzke@xceptance.com>
2024-10-02 18:47:16 +0200

    misc

---
+++ src/main/java/org/xc/jmh/Example3ArrayCopying.java
@@ -0,0 +1,65 @@
+package org.xc.jmh;
+
+import java.util.Arrays;
+import java.util.Random;
+import java.util.concurrent.TimeUnit;
+
+import org.openjdk.jmh.annotations.Benchmark;
+import org.openjdk.jmh.annotations.BenchmarkMode;
+import org.openjdk.jmh.annotations.Fork;
+import org.openjdk.jmh.annotations.Measurement;
+import org.openjdk.jmh.annotations.Mode;
+import org.openjdk.jmh.annotations.OutputTimeUnit;
+import org.openjdk.jmh.annotations.Param;
+import org.openjdk.jmh.annotations.Scope;
+import org.openjdk.jmh.annotations.Setup;
+import org.openjdk.jmh.annotations.State;
+import org.openjdk.jmh.annotations.Warmup;
+
+@State(Scope.Thread)
+@BenchmarkMode(Mode.AverageTime)
+@OutputTimeUnit(TimeUnit.NANOSECONDS)
+@Warmup(iterations = 5, time = 1, timeUnit = TimeUnit.SECONDS)
+@Measurement(iterations = 10, time = 1, timeUnit = TimeUnit.SECONDS)
+@Fork(1)
+public class Example3ArrayCopying
+{
+    @Param({"10", "1000", "100000"})
+    int SIZE;
+    byte[] src;
+
+    @Setup
+    public void setup()
+    {
+        src = new byte[SIZE];
+        final Random r = new Random(7L);
+        r.nextBytes(src);
+    }
+
+    @Benchmark
+    public byte[] systemCopy()
+    {
+        var dest = new byte[src.length];
+        System.arraycopy(src, 0, dest, 0, src.length);
+
+        return dest;
+    }
+
+    @Benchmark
+    public byte[] arrayCopy()
+    {
+        return Arrays.copyOf(src, src.length);
+    }
+
+    @Benchmark
+    public byte[] manualCopy()
+    {
+        var dest = new byte[src.length];
+        for (int i = 0; i < src.length; i++)
+        {
+            dest[i] = src[i];
+        }
+
+        return dest;
+    }
+}
