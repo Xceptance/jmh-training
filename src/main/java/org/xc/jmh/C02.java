d41f1cb27d4c41b46b4a18cdfcf573b5931a53b5
Rene Schwietzke <r.schwietzke@xceptance.com>
2024-10-05 12:18:53 +0200

    misc

--- src/main/java/org/xc/jmh/C02.java
+++ src/main/java/org/xc/jmh/C02.java
@@ -1,6 +1,5 @@
 package org.xc.jmh;
 
-import java.util.Random;
 import java.util.concurrent.TimeUnit;
 
 import org.openjdk.jmh.annotations.Benchmark;
@@ -10,95 +9,77 @@ import org.openjdk.jmh.annotations.Measurement;
 import org.openjdk.jmh.annotations.Mode;
 import org.openjdk.jmh.annotations.OutputTimeUnit;
 import org.openjdk.jmh.annotations.Scope;
-import org.openjdk.jmh.annotations.Setup;
 import org.openjdk.jmh.annotations.State;
 import org.openjdk.jmh.annotations.Warmup;
 
 /**
  *
  */
-@Measurement(iterations = 5, batchSize = 1, time=1, timeUnit = TimeUnit.NANOSECONDS)
+@Measurement(iterations = 1, time=10, timeUnit = TimeUnit.MILLISECONDS)
 @Fork(1)
 @BenchmarkMode(Mode.AverageTime)
 @OutputTimeUnit(TimeUnit.NANOSECONDS)
 @State(Scope.Thread)
 public class C02
 {
-	final int SIZE = 10000;
+	final int SIZE = 100_000;
 	final byte[] src = new byte[SIZE];
-
-	@Setup
-	public void setup()
-	{
-		final Random r = new Random(7L);
-		r.nextBytes(src);
-	}
+	final byte[] dest = new byte[SIZE];
 
 	@Benchmark
-    @Warmup(iterations = 0, batchSize = 1)
+    @Warmup(iterations = 0)
 	public byte[] systemCopyCold()
 	{
-		var dest = new byte[src.length];
 		System.arraycopy(src, 0, dest, 0, src.length);
-
 		return dest;
 	}
+
 	@Benchmark
-    @Warmup(iterations = 1, time = 100, timeUnit = TimeUnit.MILLISECONDS)
+    @Warmup(iterations = 1, time=10, timeUnit = TimeUnit.MILLISECONDS)
 	public byte[] systemCopyWarm()
 	{
-		var dest = new byte[src.length];
 		System.arraycopy(src, 0, dest, 0, src.length);
-
 		return dest;
 	}
+
 	@Benchmark
-    @Warmup(iterations = 3, time = 2, timeUnit = TimeUnit.SECONDS)
-	@Measurement(iterations = 10, batchSize = 1, time=1, timeUnit = TimeUnit.NANOSECONDS)
+    @Warmup(iterations = 1, time=1000, timeUnit = TimeUnit.MILLISECONDS)
 	public byte[] systemCopyHot()
 	{
-		var dest = new byte[src.length];
 		System.arraycopy(src, 0, dest, 0, src.length);
-
 		return dest;
 	}
 
 	@Benchmark
-    @Warmup(iterations = 0, batchSize = 1)
+    @Warmup(iterations = 0)
 	public byte[] manualCopyCold()
 	{
-		var dest = new byte[src.length];
 		for (int i = 0; i < src.length; i++)
 		{
 			dest[i] = src[i];
 		}
-
 		return dest;
 	}
+
 	@Benchmark
-    @Warmup(iterations = 1, time = 100, timeUnit = TimeUnit.MILLISECONDS)
+    @Warmup(iterations = 1, time=10, timeUnit = TimeUnit.MILLISECONDS)
 	public byte[] manualCopyWarm()
 	{
-		var dest = new byte[src.length];
 		for (int i = 0; i < src.length; i++)
 		{
 			dest[i] = src[i];
 		}
-
 		return dest;
 	}
 
 	@Benchmark
-    @Warmup(iterations = 3, time = 2, timeUnit = TimeUnit.SECONDS)
-	@Measurement(iterations = 10, batchSize = 1, time=1, timeUnit = TimeUnit.NANOSECONDS)
+    @Warmup(iterations = 1, time=1000, timeUnit = TimeUnit.MILLISECONDS)
 	public byte[] manualCopyHot()
 	{
-		var dest = new byte[src.length];
 		for (int i = 0; i < src.length; i++)
 		{
 			dest[i] = src[i];
 		}
-
 		return dest;
 	}
 }
