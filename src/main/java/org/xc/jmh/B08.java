25838cdbfc7d5134fff7e1f535e72376f2ed4e56
Rene Schwietzke <r.schwietzke@xceptance.com>
2024-10-06 15:13:37 +0200

    misc

--- src/main/java/org/xc/jmh/B08.java
+++ src/main/java/org/xc/jmh/B08.java
@@ -1,5 +1,9 @@
 package org.xc.jmh;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Random;
 import java.util.concurrent.TimeUnit;
 
 import org.openjdk.jmh.annotations.Benchmark;
@@ -8,54 +12,41 @@ import org.openjdk.jmh.annotations.Fork;
 import org.openjdk.jmh.annotations.Measurement;
 import org.openjdk.jmh.annotations.Mode;
 import org.openjdk.jmh.annotations.OutputTimeUnit;
+import org.openjdk.jmh.annotations.Param;
 import org.openjdk.jmh.annotations.Scope;
 import org.openjdk.jmh.annotations.Setup;
 import org.openjdk.jmh.annotations.State;
 import org.openjdk.jmh.annotations.Warmup;
 
 /**
- * The simplest JMH benchmark
- * Produce a result by returning data.
+ * This is horribly wrong code!!!!
  */
 @Warmup(iterations = 3, time = 1, timeUnit = TimeUnit.SECONDS)
-@Measurement(iterations = 3, time = 1, timeUnit = TimeUnit.SECONDS)
+@Measurement(iterations = 5, time = 1, timeUnit = TimeUnit.SECONDS)
 @Fork(1)
 @BenchmarkMode(Mode.AverageTime)
 @OutputTimeUnit(TimeUnit.NANOSECONDS)
 @State(Scope.Thread)
-public class A04a
+public class B08
 {
-	long time;
-	Long lTime;
+	@Param({"1", "10", "100", "1000"})
+	int SIZE;
+	List<String> data = new ArrayList<>(SIZE);
 
 	@Setup
 	public void setup()
 	{
-		time = System.currentTimeMillis();
-		lTime = Long.valueOf(time);
+		var r = new Random(42);
+		for (int i = 0; i < SIZE; i++)
+		{
+			data.add(String.valueOf(r.nextInt()));
+		}
 	}
 
 	@Benchmark
-	public long addCheap1()
+	public List<String> sort()
 	{
-		return 1 + 1727610292048L;
-	}
-
-	@Benchmark
-	public long addCheap2()
-	{
-		return 1 + time;
-	}
-
-	@Benchmark
-	public long addCheap3()
-	{
-		return 1 + lTime.longValue();
-	}
-
-	@Benchmark
-	public double addExpensive()
-	{
-		return (time * 0x5DEECE66DL + 0xBL + time) / (time * time * 0.42d);
+		Collections.sort(data);
+		return data;
 	}
 }
