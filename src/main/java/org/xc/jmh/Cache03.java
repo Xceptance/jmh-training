ac4a09e35a30f6fa270af02c1493c279f063117c
Rene Schwietzke <r.schwietzke@xceptance.com>
2024-10-07 00:27:01 +0200

    misc

--- src/main/java/org/xc/jmh/Cache03.java
+++ src/main/java/org/xc/jmh/Cache03.java
@@ -16,7 +16,7 @@ import org.openjdk.jmh.annotations.Warmup;
 @State(Scope.Thread)
 @BenchmarkMode(Mode.AverageTime)
 @OutputTimeUnit(TimeUnit.NANOSECONDS)
-@Warmup(iterations = 3, time = 1, timeUnit = TimeUnit.SECONDS)
+@Warmup(iterations = 6, time = 1, timeUnit = TimeUnit.SECONDS)
 @Measurement(iterations = 3, time = 1, timeUnit = TimeUnit.SECONDS)
 @Fork(1)
 public class Cache03
