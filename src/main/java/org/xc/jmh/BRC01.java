1dbcc9c38553b0e8b683c6f70475a25150b66635
Rene Schwietzke <r.schwietzke@xceptance.com>
2024-10-08 15:08:39 +0200

    misc

--- src/main/java/org/xc/jmh/BRC01.java
+++ src/main/java/org/xc/jmh/BRC01.java
@@ -12,6 +12,7 @@ import org.openjdk.jmh.annotations.Measurement;
 import org.openjdk.jmh.annotations.Mode;
 import org.openjdk.jmh.annotations.OperationsPerInvocation;
 import org.openjdk.jmh.annotations.OutputTimeUnit;
+import org.openjdk.jmh.annotations.Param;
 import org.openjdk.jmh.annotations.Scope;
 import org.openjdk.jmh.annotations.Setup;
 import org.openjdk.jmh.annotations.State;
@@ -22,13 +23,19 @@ import org.xc.jmh.util.ParseDouble;
  * Just some BRC number parsing fun
  */
 @Warmup(iterations = 10, time = 1, timeUnit = TimeUnit.SECONDS)
-@Measurement(iterations = 5, time = 1, timeUnit = TimeUnit.SECONDS)
+@Measurement(iterations = 10, time = 1, timeUnit = TimeUnit.SECONDS)
 @Fork(value = 1, jvmArgsAppend = {"-Xms4g", "-Xmx4g", "-XX:+AlwaysPreTouch"})
 @BenchmarkMode(Mode.AverageTime)
 @OutputTimeUnit(TimeUnit.NANOSECONDS)
 @State(Scope.Thread)
 public class BRC01
 {
+	@Param({"true", "false"})
+	boolean negativeOnly;
+
+	@Param({"true", "false"})
+	boolean small;
+
 	final int SIZE = 10_000;
 	List<String> sNumbers = new ArrayList<>(SIZE);
 	List<byte[]> bNumbers = new ArrayList<>(SIZE);
@@ -37,11 +44,17 @@ public class BRC01
 	public void setup()
 	{
 		var r = new Random(4242L);
+
+		var range = small ? 10 : 100;
+
 		for (int i = 0; i < SIZE; i++)
 		{
-			var a = r.nextInt(199) - 99; // -99 to 99
+			var a = r.nextInt(range); // -99 to 99
 			var b = r.nextInt(10); // 0 to 9
 			var s = String.valueOf(a) + "." + String.valueOf(b);
+
+			s = negativeOnly || r.nextBoolean() ? "-" + s : s;
+
 			sNumbers.add(s);
 			bNumbers.add(s.getBytes());
 		}
